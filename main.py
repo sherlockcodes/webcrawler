2322102b4ddfcc2d80c365094b7643d9e55d5cdb
hari <hariteritree@gmail.com>
2017-02-12 12:33:49 +0530

    adding overcart config file

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ print 'total categories', len(categories) , ' in ' + url
 time.sleep(5)
 for category in categories:
   if "name" in category:
-  	print 'getting products for ', category["name"]
+  	print 'getting products for ', category["url"]
   products = store.get_products(category)
-  print 'totally ' + str(len(products)) + ' products in the ' + category["name"] + " category"
+  print 'totally ' + str(len(products)) + ' products in the ' + str(category["name"])+ " category"
   time.sleep(3)
   for product in products:
   	print product
